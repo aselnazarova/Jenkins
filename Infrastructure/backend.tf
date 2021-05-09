8a535c4dbf7cf9c64f1d8aa7dbef5a4474486b70
aselnazarova <35941086+aselnazarova@users.noreply.github.com>
2021-05-09 12:51:53 -0500

    Update backend.tf

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-file-asel"
-    key    = "tfstate/jenkins-pipeline.tfstate"
+    key    = "tfstate/jenkins-pipeline2.tfstate"
     region = "us-east-1"
   }
 }
